4319f0b060f48af214aa23f42cd6370c51e0d16a
pavelKuklin <pavelkuklinweb@gmail.com>
2020-03-13 11:04:06 +0500

    модальные окна

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,5 +1,35 @@
 const popup = () => {
-    console.log('popup');
+    const body = document.querySelector('body'),
+        freeVisitForm = document.getElementById('free_visit_form'),
+        callbackForm = document.getElementById('callback_form');
+
+    const switchPopup = (selector) => {
+        selector.classList.toggle('show');
+        body.classList.toggle('scroll-hidden');
+    }
+
+    const openPopup = () => {
+        let target = event.target;
+
+        if (target.getAttribute('data-popup')) {
+            switchPopup(document.querySelector(target.getAttribute('data-popup')))
+        }
+        if (target.closest('.close-form')) {
+            target = target.closest('.close-form');
+            switchPopup(target.parentElement.parentElement);
+        }
+        console.log(callbackForm.classList.contains('show'));
+
+        //закрываем модалки при нажатии мимо    
+        if (callbackForm.classList.contains('show') && !target.getAttribute('data-popup') && !target.closest('.form-content')) {
+            switchPopup(callbackForm);
+        } else if (freeVisitForm.classList.contains('show') && !target.getAttribute('data-popup') && !target.closest('.form-content')) {
+            switchPopup(freeVisitForm);
+        }
+    };
+
+
+    body.addEventListener('click', openPopup);
 
 }
 
